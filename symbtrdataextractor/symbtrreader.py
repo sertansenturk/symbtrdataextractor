327f64d049c5dc6d4af24e9e0679414af6f3bf2f
Sertan Senturk <sertansenturk_mtg@Sertans-MTG-MacBook-Pro.local>
2015-10-28 12:08:34 +0100

    Added mu2 header reader

--- symbtrdataextractor/symbtrreader.py
+++ symbtrdataextractor/symbtrreader.py
@@ -1,5 +1,6 @@
 import csv
 import string
+import pdb
 
 def readTxtScore(scorefile):
     with open(scorefile, "rb") as f:
@@ -35,4 +36,42 @@ def readTxtScore(scorefile):
 
     return score
 
+def readMu2Score(scorefile):
+    # TODO
+    pass
 
+def readMu2Header(scorefile):
+    with open(scorefile, "rb") as f:
+        reader = csv.reader(f, delimiter='\t')
+
+        header_row = next(reader, None)
+
+        header = dict()
+        for row in reader:
+            code = int(row[0])
+            if code == 50:
+                header['makam'] = {'mu2_name':row[7]}
+                header['key_signature'] = row[8].split('/')
+            elif code == 51:
+                header['usul'] = {'mu2_name':row[7], 'mertebe':int(row[3]), 
+                    'number_of_pulses':int(row[2])}
+            elif code == 52:
+                header['tempo'] = {'value':int(row[4]), 'unit':'bpm'}
+                if not int(row[3]) == header['usul']['mertebe']:
+                    print '   Mertebe and tempo unit are different!'
+            elif code == 57:
+                header['form'] = {'mu2_name':row[7]}
+            elif code == 58:
+                header['composer'] = {'mu2_name':row[7]}
+            elif code == 59:
+                header['lyricist'] = {'mu2_name':row[7]}
+            elif code == 60:
+                header['mu2_title'] = row[7]
+            elif code == 63:
+                header['genre'] = row[7]
+            elif code in range(50, 64): 
+                print '   Unparsed code: ' + ' '.join(row)
+            else:  # end of header
+                break
+
+    return header
