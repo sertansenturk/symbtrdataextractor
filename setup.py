f9e6094212cf6f0a4e133ce06f097025e8a533f2
Sertan Senturk <contact@sertansenturk.com>
2016-03-15 09:55:59 +0100

    Refined requirements

--- setup.py
+++ setup.py
@@ -3,12 +3,17 @@
 from setuptools import setup
 
 setup(name='symbtrdataextractor',
-      version='2.0.0-alpha.1',
-      author='Sertan Senturk',
-      author_email='contact AT sertansenturk DOT com',
-      license='agpl 3.0',
-      description='Tools to extract (meta)data related to SymbTr from the information in score file and MusicBrainz',
-      url='http://sertansenturk.com',
-      packages=['symbtrdataextractor'],
-      include_package_data=True,
+    version='2.0.0-alpha.1',
+    author='Sertan Senturk',
+    author_email='contact AT sertansenturk DOT com',
+    license='agpl 3.0',
+    description='Tools to extract (meta)data related to SymbTr from the information in score file and MusicBrainz',
+    url='http://sertansenturk.com',
+    packages=['symbtrdataextractor'],
+    include_package_data=True,
+    install_requires=[
+        "python-Levenshtein",
+        "networkx",
+        "numpy"
+    ],
 )
