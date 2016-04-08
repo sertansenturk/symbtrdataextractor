9f2321ee5beef4e828bc916c94d183c8fccab9bb
Sertan Senturk <contact@sertansenturk.com>
2016-04-08 23:35:30 +0200

    Moved unittests inside the package

--- symbtrdataextractor/unittests/extractor_test.py
+++ symbtrdataextractor/unittests/extractor_test.py
@@ -3,19 +3,24 @@ from symbtrdataextractor.SymbTrReader import SymbTrReader
 import json
 import os
 
+_curr_folder = os.path.dirname(os.path.abspath(__file__))
+
 
 def test_extractor():
     # inputs
     scorename = 'kurdilihicazkar--sarki--agiraksak--ehl-i_askin--tatyos_efendi'
 
-    txt_filename = os.path.join('sampledata', scorename + '.txt')
+    txt_filename = os.path.join(_curr_folder, '..', '..', 'sampledata',
+                                scorename + '.txt')
 
     mbid = 'b43fd61e-522c-4af4-821d-db85722bf48c'
 
-    auto_seg_file = os.path.join('sampledata', scorename + '.autoSeg')
+    auto_seg_file = os.path.join(_curr_folder, '..', '..', 'sampledata',
+                                 scorename + '.autoSeg')
     auto_seg_bounds = json.load(open(auto_seg_file, 'r'))['boundary_noteIdx']
 
-    mu2_filename = os.path.join('sampledata', scorename + '.mu2')
+    mu2_filename = os.path.join(_curr_folder, '..', '..', 'sampledata',
+                                scorename + '.mu2')
 
     # initialize the extractor
     extractor = SymbTrDataExtractor(
@@ -35,7 +40,7 @@ def test_extractor():
     data = SymbTrDataExtractor.merge(txt_data, mu2_header)
 
     # compare with a previously saved result
-    score_data_file = os.path.join('unittests', scorename + '.json')
+    score_data_file = os.path.join(_curr_folder, scorename + '.json')
     saved_data = json.load(open(score_data_file))
 
     assert saved_data == data
