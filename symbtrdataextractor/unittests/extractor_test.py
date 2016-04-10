550b6706ae104e7627fc91ba28cc4280dda22059
Sertan Senturk <contact@sertansenturk.com>
2016-04-11 00:18:04 +0200

    Added new unittests

--- symbtrdataextractor/unittests/extractor_test.py
+++ symbtrdataextractor/unittests/extractor_test.py
@@ -7,21 +7,48 @@ from symbtrdataextractor.reader.Mu2Reader import Mu2Reader
 _curr_folder = os.path.dirname(os.path.abspath(__file__))
 
 
-def test_extractor():
+def test_with_vocal_section_starting_mid_measure():
+    """
+    Tests the result with the score of a vocal composition in which some of
+    the lyrical lines start in middle of the measure
+    """
+    # inputs
+    scorename = 'hicaz_humayun--beste--hafif--olmada_diller--abdulhalim_aga'
+
+    txt_filename = os.path.join(_curr_folder, 'data', scorename + '.txt')
+
+    # initialize the extractor
+    extractor = SymbTrDataExtractor(
+        extract_all_labels=False, melody_sim_thres=0.75, lyrics_sim_thres=0.75,
+        get_recording_rels=False, print_warnings=True)
+
+    # extract txt_data
+    txt_data, is_data_valid = extractor.extract(txt_filename,
+                                                symbtr_name=scorename)
+
+    # compare with a previously saved result
+    score_data_file = os.path.join(_curr_folder, 'data', scorename + '.json')
+    saved_data = json.load(open(score_data_file))
+
+    assert saved_data == txt_data and is_data_valid
+
+
+def test_full_input():
+    """
+    Tests the result with complete information available, i.e. mbid, phrase
+    annotation and user provided segmentation
+    """
     # inputs
     scorename = 'kurdilihicazkar--sarki--agiraksak--ehl-i_askin--tatyos_efendi'
 
-    txt_filename = os.path.join(_curr_folder, '..', '..', 'sampledata',
-                                scorename + '.txt')
+    txt_filename = os.path.join(_curr_folder, 'data', scorename + '.txt')
 
     mbid = 'b43fd61e-522c-4af4-821d-db85722bf48c'
 
-    auto_seg_file = os.path.join(_curr_folder, '..', '..', 'sampledata',
-                                 scorename + '.autoSeg')
+    auto_seg_file = os.path.join(_curr_folder, 'data', scorename + '.autoSeg')
     auto_seg_bounds = json.load(open(auto_seg_file, 'r'))['boundary_noteIdx']
 
-    mu2_filename = os.path.join(_curr_folder, '..', '..', 'sampledata',
-                                scorename + '.mu2')
+    mu2_filename = os.path.join(_curr_folder, 'data', scorename + '.mu2')
 
     # initialize the extractor
     extractor = SymbTrDataExtractor(
@@ -39,9 +66,10 @@ def test_extractor():
 
     # merge
     data = SymbTrDataExtractor.merge(txt_data, mu2_header)
+    is_valid = is_data_valid and is_header_valid
 
     # compare with a previously saved result
-    score_data_file = os.path.join(_curr_folder, scorename + '.json')
+    score_data_file = os.path.join(_curr_folder, 'data', scorename + '.json')
     saved_data = json.load(open(score_data_file))
 
-    assert saved_data == data
+    assert saved_data == data and is_valid
