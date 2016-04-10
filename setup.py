90278c30c305049a3260ea92b0a35bc4791d217d
Sertan Senturk <contact@sertansenturk.com>
2016-04-10 18:18:39 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='symbtrdataextractor',
-      version='2.0.0-alpha.3',
+      version='2.0.0-alpha.4',
       author='Sertan Senturk',
       author_email='contact AT sertansenturk DOT com',
       license='agpl 3.0',
