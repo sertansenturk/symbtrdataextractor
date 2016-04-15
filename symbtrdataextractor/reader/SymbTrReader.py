55032107a03474a6638c0843e6467a9528c5c97a
Sertan Senturk <contact@sertansenturk.com>
2016-04-15 02:58:13 +0200

    Added symbtr_name checker in SymbTrReader

--- symbtrdataextractor/reader/SymbTrReader.py
+++ symbtrdataextractor/reader/SymbTrReader.py
@@ -5,3 +5,7 @@ class SymbTrReader(object):
     @staticmethod
     def get_symbtr_name_from_filepath(score_file, symbtr_name):
         return os.path.splitext(os.path.basename(score_file))[0]
+
+    @staticmethod
+    def is_symbtr_name(in_str):
+        return len('--'.split(in_str)) == 5
